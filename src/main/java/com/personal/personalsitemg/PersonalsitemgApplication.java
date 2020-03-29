67a97a322d56582e4f55cec2ac9c5cc9b365c776
MelindaGreene <Melindagreene711@gmail.com>
2020-03-28 20:27:53 -0500

    start of project

---
+++ src/main/java/com/personal/personalsitemg/PersonalsitemgApplication.java
@@ -0,0 +1,13 @@
+package com.personal.personalsitemg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalsitemgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalsitemgApplication.class, args);
+    }
+
+}
